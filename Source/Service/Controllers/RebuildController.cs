fbc7fb74624a2eec92f0bcda890696ca5af3c940
Mohit Sengar <mohitsingh.shiv@gmail.com>
2020-12-22 23:25:22 +0530

    s3tos3 api added

--- Source/Service/Controllers/RebuildController.cs
+++ Source/Service/Controllers/RebuildController.cs
@@ -302,6 +302,141 @@ namespace Glasswall.CloudSdk.AWS.Rebuild.Controllers
             }
         }
 
+        [HttpPost("s3tos3")]
+        public async Task<IActionResult> RebuildFromFormS3ToS3([FromForm] string contentManagementFlagJson, [FromForm][Required] string sourcePresignedURL, [FromForm][Required] string targetPresignedURL)
+        {
+            string uploads = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+            string tempFolderPath = Path.Combine(uploads, Guid.NewGuid().ToString());
+
+            try
+            {
+                Logger.LogInformation("'{0}' method invoked", nameof(RebuildFromFormS3ToZipFile));
+
+                ContentManagementFlags contentManagementFlags = null;
+                if (!string.IsNullOrWhiteSpace(contentManagementFlagJson))
+                    contentManagementFlags = await Task.Run(() => Newtonsoft.Json.JsonConvert.DeserializeObject<ContentManagementFlags>(contentManagementFlagJson));
+
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+
+                AmazonS3Client amazonS3Client = new AmazonS3Client(Environment.GetEnvironmentVariable("AWS_ACCESS_KEY_ID"), Environment.GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY"), RegionEndpoint.EUWest1);
+                AmazonS3Uri amazonS3Uri = new AmazonS3Uri(sourcePresignedURL);
+                GetObjectRequest request = new GetObjectRequest()
+                {
+                    BucketName = amazonS3Uri.Bucket,
+                    Key = amazonS3Uri.Key
+                };
+
+                GetObjectResponse s3objectResponse = await amazonS3Client.GetObjectAsync(request);
+
+                MemoryStream memStream = new MemoryStream();
+                s3objectResponse.ResponseStream.CopyTo(memStream);
+                memStream.Seek(0, SeekOrigin.Begin);
+                IFormFile file = new FormFile(memStream, 0, memStream.Length, null, Path.GetFileName(amazonS3Uri.Key));
+                if (!TryReadFormFile(file, out var fileBytes))
+                    return BadRequest("Input file could not be read.");
+
+                RecordEngineVersion();
+
+                FileTypeDetectionResponse fileType = await Task.Run(() => DetectFromBytes(fileBytes));
+
+                if (fileType.FileType != FileType.Zip)
+                    return UnprocessableEntity("Input file could not be processed.");
+
+                string zipFolderName = $"{Guid.NewGuid()}";
+                string protectedZipFolderPath = Path.Combine(tempFolderPath, Guid.NewGuid().ToString());
+                string zipFolderPath = Path.Combine(tempFolderPath, zipFolderName);
+                string zipFilePath = $"{zipFolderPath}.{fileType.FileTypeName}";
+                if (!Directory.Exists(uploads))
+                {
+                    Directory.CreateDirectory(uploads);
+                }
+
+                if (!Directory.Exists(tempFolderPath))
+                {
+                    Directory.CreateDirectory(tempFolderPath);
+                }
+
+                if (!Directory.Exists(protectedZipFolderPath))
+                {
+                    Directory.CreateDirectory(protectedZipFolderPath);
+                }
+
+                using (Stream fileStream = new FileStream(zipFilePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+
+                ZipFile.ExtractToDirectory(zipFilePath, zipFolderPath, true);
+                foreach (var directory in Directory.EnumerateDirectories(zipFolderPath))
+                {
+                    if (directory.EndsWith("__MACOSX"))
+                    {
+                        continue;
+                    }
+
+                    foreach (var extractedFile in Directory.GetFiles(directory))
+                    {
+                        using FileStream stream = System.IO.File.OpenRead(extractedFile);
+                        IFormFile iFormFile = new FormFile(stream, 0, stream.Length, null, Path.GetFileName(stream.Name));
+
+                        if (!TryReadFormFile(iFormFile, out fileBytes))
+                            return BadRequest("Input file could not be read.");
+
+                        fileType = await Task.Run(() => DetectFromBytes(fileBytes));
+
+                        if (fileType.FileType == FileType.Unknown)
+                            return UnprocessableEntity("Input file could not be processed.");
+
+                        IFileProtectResponse protectedFileResponse = await Task.Run(() => RebuildFromBytes(
+                        contentManagementFlags, fileType.FileTypeName, fileBytes));
+
+                        if (!string.IsNullOrWhiteSpace(protectedFileResponse.ErrorMessage))
+                        {
+                            if (protectedFileResponse.IsDisallowed)
+                                return Ok(protectedFileResponse);
+
+                            return UnprocessableEntity(
+                                $"File could not be rebuilt. Error Message: {protectedFileResponse.ErrorMessage}");
+                        }
+
+                        System.IO.File.WriteAllBytes(Path.Combine(protectedZipFolderPath, Path.GetFileName(extractedFile)), protectedFileResponse.ProtectedFile);
+                    }
+                }
+
+                ZipFile.CreateFromDirectory(protectedZipFolderPath, $"{protectedZipFolderPath}.{FileType.Zip}");
+                using (Stream fs = System.IO.File.OpenRead($"{protectedZipFolderPath}.{FileType.Zip}"))
+                {
+                    AmazonS3Uri amazonS3TargetUri = new AmazonS3Uri(targetPresignedURL);
+                    PutObjectRequest putRequest = new PutObjectRequest()
+                    {
+                        InputStream = fs,
+                        BucketName = amazonS3TargetUri.Bucket,
+                        Key = amazonS3TargetUri.Key
+                    };
+
+                    PutObjectResponse response = await amazonS3Client.PutObjectAsync(putRequest);
+                    await memStream.DisposeAsync();
+                    if (response.HttpStatusCode != System.Net.HttpStatusCode.OK)
+                    {
+                        return BadRequest("S3 target updation failed.");
+                    }
+                }
+
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                Logger.LogError(e, $"Exception occured processing file: {e.Message}");
+                throw;
+            }
+            finally
+            {
+                if (Directory.Exists(tempFolderPath))
+                    Directory.Delete(tempFolderPath, true);
+            }
+        }
+
         [HttpPost("base64")]
         public async Task<IActionResult> RebuildFromBase64([FromBody][Required]Base64Request request)
         {
