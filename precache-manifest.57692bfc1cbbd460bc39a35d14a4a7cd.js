81d509a26192d98238a49f7cf491d24203682da0
Dinis Cruz <dinis.cruz@owasp.org>
2020-12-22 20:28:01 +0000

    Adding swagger for file drop

---
+++ precache-manifest.57692bfc1cbbd460bc39a35d14a4a7cd.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57ab7615f13f29693ac2c798dd0e975",
+    "url": "/cloudsdk-aws-rebuild-api/index.html"
+  },
+  {
+    "revision": "a2b2a6f3bdda060da3bb",
+    "url": "/cloudsdk-aws-rebuild-api/static/css/2.ecb7c289.chunk.css"
+  },
+  {
+    "revision": "030835cfbf09f008c8d6",
+    "url": "/cloudsdk-aws-rebuild-api/static/css/main.78ff68bb.chunk.css"
+  },
+  {
+    "revision": "a2b2a6f3bdda060da3bb",
+    "url": "/cloudsdk-aws-rebuild-api/static/js/2.9e3644e6.chunk.js"
+  },
+  {
+    "revision": "cccc6d91b5e64164365f6c4135751a26",
+    "url": "/cloudsdk-aws-rebuild-api/static/js/2.9e3644e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "030835cfbf09f008c8d6",
+    "url": "/cloudsdk-aws-rebuild-api/static/js/main.41c931e5.chunk.js"
+  },
+  {
+    "revision": "59b9786a1fc2ec8c3c37",
+    "url": "/cloudsdk-aws-rebuild-api/static/js/runtime-main.f8199be4.js"
+  },
+  {
+    "revision": "98cd2e7fc7ca52a8ee3ad939e298369f",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/api.98cd2e7f.yaml"
+  },
+  {
+    "revision": "8503c64fa486375dc20b3c00aa2e250a",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/download-icon-white.8503c64f.svg"
+  },
+  {
+    "revision": "6332dd8cb99e02f089de4f747566362c",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/drop-grey.6332dd8c.svg"
+  },
+  {
+    "revision": "813b1258bed1257b91bcff7bf0282fbc",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/drop-white.813b1258.svg"
+  },
+  {
+    "revision": "34536746f7b8c5b46d8675f1941e1dbc",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/info-icon.34536746.svg"
+  },
+  {
+    "revision": "6c8e57277f41d456ad53ae6fcbc74b80",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/logo.6c8e5727.svg"
+  },
+  {
+    "revision": "d41c631875217e983fe2051e2c62932e",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/modal-close.d41c6318.svg"
+  },
+  {
+    "revision": "b01fa7f07a4c2e355eff2be5069f4c93",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/montserrat-regular-webfont.b01fa7f0.woff2"
+  },
+  {
+    "revision": "e8b7d8acf640a69f838ca38c063e33ff",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/montserrat-regular-webfont.e8b7d8ac.woff"
+  },
+  {
+    "revision": "9d9e09d7ae09f64a8a365e669105bedd",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/roboto-regular-webfont.9d9e09d7.woff"
+  },
+  {
+    "revision": "d96d07c1e0f7cdf7a1988dcb0bf6e0e4",
+    "url": "/cloudsdk-aws-rebuild-api/static/media/roboto-regular-webfont.d96d07c1.woff2"
+  }
+]);
